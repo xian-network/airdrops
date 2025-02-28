a2f925cee3b013a31309d5fe4e88966308edc04c
crosschainer <eodkwokm@gmail.com>
2025-02-28 14:57:18 +0000

    ux

--- js/main.js
+++ js/main.js
@@ -121,3 +121,15 @@ elements.claimRocketswap.addEventListener('click', () => {
 elements.claimButton.addEventListener('click', claim);
 
 
+// Burger menu
+document.addEventListener('DOMContentLoaded', () => {
+    const burger = document.getElementById('burger');
+    const menu = document.getElementById('navbarBasicExample');
+
+    if (burger && menu) {
+        burger.addEventListener('click', () => {
+            burger.classList.toggle('is-active');
+            menu.classList.toggle('is-active');
+        });
+    }
+});
